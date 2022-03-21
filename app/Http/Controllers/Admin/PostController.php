ec4dedf695f6f0a38e230b3a56c60916bfbee284
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-03-21 17:34:49 +0100

    post controller

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
 
 class PostController extends Controller
 {
@@ -25,7 +27,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
@@ -36,7 +39,29 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:50',
+            'author' => 'required|string|max:50',
+            'content' => 'required|string|unique:posts',
+        ]);
+
+        $form_data = $request->all();
+
+        $slugTmp = Str::slug($form_data['title']);
+
+        $count = 1;
+
+        while(Post::where('slug',$slugTmp)->first()){
+            $slugTmp = Str::slug($form_data['title']).'-'.$count;
+            $count ++;
+        }
+
+        $form_data['slug'] = $slugTmp;
+
+        $newPost = new Post();
+
+        $newPost = Post::create($form_data);
+        return redirect()->route('admin.posts.show', $newPost->id);
     }
 
     /**
@@ -45,9 +70,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
@@ -56,9 +81,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        $categories = Category::all();
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
@@ -68,9 +94,36 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:50',
+            'author' => 'required|string|max:50',
+            'content' => 'required|string',
+        ]);
+
+        $form_data=$request->all();
+
+        if($post->title == $form_data['title']){
+            $slug = $post->slug;
+        }else{
+            $slug = Str::slug($form_data['title']);
+            $count = 1;
+            while(Post::where('slug',$slug)
+                ->where('id','!=',$post->id)
+                ->first()){
+                    $slug = Str::slug($form_data['title']).'-'.$count;
+                    $count ++;
+                }
+            }
+        $form_data['slug'] = $slug; 
+
+
+
+
+        $post->update($form_data);
+
+        return redirect()->route('admin.posts.show', $post->id);
     }
 
     /**
@@ -79,8 +132,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route("admin.posts.index");
     }
 }
